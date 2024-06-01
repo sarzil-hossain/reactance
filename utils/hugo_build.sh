291d8da3011d9ec21b0bc8505b5bbb42ab7fd0d5
repliqa <sarzilhossain6@gmail.com>
2024-06-02 01:24:03 +0600

    just removed the directory check

--- utils/hugo_build.sh
+++ utils/hugo_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-[ $(pwd | rev | cut -d '/' -f 1 | rev) != "reactance" ] && echo Please run from reactance directory && exit 1
 
 for d in .hugo_sites_build/*; do
 	uname=$(echo $d | rev | cut -d / -f 1 | rev)
